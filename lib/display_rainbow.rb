3b56a6cc05bef602627cc93e828b33bd937d9aa3
Harleigh Abel <harleighabel@gmail.com>
2018-01-24 00:43:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,9 +1,9 @@
 def display_rainbow(colors)
   n = 0
-  while n > 7 do
+  while n > 7 {
     puts "#{colors[n]}"
     n += 1
-  end
+  }
 end
 
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
